9481147ee67576ff81cacbe4147e879a6f4ba942
FunkySphere <krauzowiczs42@gmail.com>
2023-03-20 15:29:53 +0100

    title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'github-portfolio-website';
+  title = 'Front-end Dev - Sebastian Krauzowicz';
 }
